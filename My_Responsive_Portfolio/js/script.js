132c9b761da09e87ad7fc18ad3690feced9544a4
Mahesh <161201753+Mahesh20405@users.noreply.github.com>
2024-07-27 07:44:01 +0530

    Update script.js

--- My_Responsive_Portfolio/js/script.js
+++ My_Responsive_Portfolio/js/script.js
@@ -12,31 +12,9 @@ let typed = new Typed(".typing", {
   loop: true,
 });
 
-/* ========================= Aside ========================= */
-const nav = document.querySelector(".nav"),
-  navList = nav.querySelectorAll("li"),
-  totalNavList = navList.length,
-  allSection = document.querySelectorAll(".section"),
-  totalSection = allSection.length;
 
-for (let i = 0; i < totalNavList; i++) {
-  const a = navList[i].querySelector("a");
-  a.addEventListener("click", function () {
-    removeBackSection();
-    for (let j = 0; j < totalNavList; j++) {
-      if (navList[j].querySelector("a").classList.contains("active")) {
-        addBackSection(j);
-        //allSection[j].classList.add("back-section")
-      }
-      navList[j].querySelector("a").classList.remove("active");
-    }
-    this.classList.add("active");
-    showSection(this);
-    if (window.innerWidth < 1200) {
-      asideSectionTogglerBtn();
-    }
-  });
-}
+/* ========================= **Code Snipped** ========================= */
+
 
 function removeBackSection() {
   for (let i = 0; i < totalSection; i++) {
